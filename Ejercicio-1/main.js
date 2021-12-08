03e3dcaaa9dc8959a9a464fcfb3f0585ecfe931f
Juan de Dios López Gálvez <juandedioslopgal@gmail.com>
2021-12-08 09:47:29 -0600

    Ejercicio 1 Completo

---
+++ Ejercicio-1/main.js
@@ -0,0 +1,15 @@
+let nombre = prompt("Ingrese su primer nombre");
+let apellido = prompt("Ingrese su primer apellido");
+
+let num1 = parseInt(prompt("Ingrese un primer número entero"));
+let num2 = parseInt(prompt("Ingrese un segundo número entero"));
+
+let suma = num1 + num2;
+let multi = num1 * num2;
+let resultado = "Bienvenido "+ nombre + " " + apellido + 
+                " La suma de sus números es de: "+ suma + " y su multiplicacion es de: " + multi;
+alert(resultado);
+
+
+
+
