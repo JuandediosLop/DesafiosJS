88ccbafea04f36228a8ff0fd64dd852142bfd06c
Juan de Dios López Gálvez <juandedioslopgal@gmail.com>
2021-12-08 10:02:08 -0600

    Ejercicio 2 completo

---
+++ Ejercicio-2/main.js
@@ -0,0 +1,8 @@
+
+let edad= parseInt(prompt("Ingrese tu edad"));
+
+if (edad > 17 ){
+    alert("Tiene "+ edad +" años, es mayor de edad")
+} else{
+    alert("Tiene "+ edad +" años, es menor de edad")
+}
