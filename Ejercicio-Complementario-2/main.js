62a9e2143ffcf9d4fd2d7066f4e78a3bcf351655
Juan de Dios López Gálvez <juandedioslopgal@gmail.com>
2021-12-25 22:49:42 -0600

    Complementario 2 completo

--- Ejercicio-Complementario-2/main.js
+++ Ejercicio-Complementario-2/main.js
@@ -1,15 +1,26 @@
 let productos = [];
+class Producto{
+    constructor(nombre, cantidad, categoria){
+      this.nombre = nombre;
+      this.cantidad = cantidad;
+      this.categoria = categoria;
+    }
+}
 
 function agregarProducto(){
 
     let conteoProducto = parseInt( prompt("¿Cuantos productos desea añadir? \n Ingrese un valor númerico"));
 
     for(let index = 1; index <= conteoProducto; index++ ){
-        let ingresarProductos = prompt("Escribe el nombre del producto a ingresar: \n" + index + "/" + conteoProducto).toLowerCase();
-        productos.push(ingresarProductos); 
+        alert("Bienvenido, por favor ingresa la información que se solicita del producto \nProducto:" +index +  " de " + conteoProducto)
+        productos.push( new Producto(
+        prompt('Ingrese el nombre').toLowerCase(),
+        parseInt(prompt('Ingrese la cantidad de producto \n Ingrese un valor númerico')),
+        prompt('Ingrese la categoria').toLowerCase()
+        ));
+        
     }
-    alert("Lo redirigimos a ordenar su array");
-        ordenarArray(obtenerFormaOrden());
+        ordenarArray(obtenerFormaOrden(), modoDeOrden());
 }
 
 function obtenerFormaOrden(){
@@ -20,27 +31,65 @@ function reordenar(){
     let reordenarProducto = prompt("¿Desea ordenar otra vez sus productos?").toLowerCase();
     
     if(reordenarProducto == "si"){
-        ordenarArray(obtenerFormaOrden());
+        
+        ordenarArray(obtenerFormaOrden(), modoDeOrden());
     } else {
-        alert("Adios")
+        alert("Adios");
     }
 }
 
-function ordenarArray(tipoOrdenar){
+function modoDeOrden(){
+    return prompt("Ingrese el indice que desea ordenar sus productos: \n Opciones: nombre, cantidad, categoria").toLowerCase();
+}
 
+function ordenarArray(tipoOrdenar, modoOrden){
     if (tipoOrdenar == "ascendente"){
-        productos.sort((a, b) => a > b ? 1 : -1);
-        alert("Sus productos ascedentemente \n" +productos);
-        reordenar();
 
+        if(modoOrden == "nombre"){
+            productos.sort((a, b) => a.nombre> b.nombre ? 1 : -1);
+            alert("Sus productos en orden " + tipoOrdenar +" y por " + modoOrden + "\n"  + JSON.stringify(productos));
+            reordenar(); 
+        } else if (modoOrden =="cantidad"){
+            productos.sort((a, b) => a.cantidad> b.cantidad ? 1 : -1);
+            alert("Sus productos en orden " + tipoOrdenar +" y por " + modoOrden + "\n"  + JSON.stringify(productos));
+            reordenar(); 
+        } else if(modoOrden ==="categoria"){
+            productos.sort((a, b) => a.categoria> b.categoria ? 1 : -1);
+            alert("Sus productos en orden " + tipoOrdenar +" y por " + modoOrden + "\n"  + JSON.stringify(productos));
+            reordenar();
+        }else{
+            alert('Valor no valido, intente de nuevo');
+            ordenarArray(obtenerFormaOrden(), modoDeOrden());
+        }
+        
     } else if (tipoOrdenar == "descendente"){
-        productos.sort((a, b) => a > b ? -1 : 1);
-        alert("Sus productos descendentemente\n" + productos);
-        reordenar();
+
+        if(modoOrden =="nombre"){
+            productos.sort((a, b) => a.nombre > b.nombre ? -1 : 1);
+            alert("Sus productos en orden " + tipoOrdenar +" y por " + modoOrden + "\n"  + JSON.stringify(productos));
+            reordenar();
+
+        }else if(modoOrden == "cantidad"){
+            productos.sort((a, b) => a.cantidad > b.cantidad ? -1 : 1);
+            alert("Sus productos en orden " + tipoOrdenar +" y por " + modoOrden + "\n"  + JSON.stringify(productos));
+            reordenar();
+
+        }else if(modoOrden == "categoria"){
+            productos.sort((a, b) => a.categoria > b.categoria ? -1 : 1);
+            alert("Sus productos en orden " + tipoOrdenar +" y por " + modoOrden + "\n"  + JSON.stringify(productos));
+            reordenar();
+        }else{
+            alert('Valor no valido, intente de nuevo');
+            ordenarArray(obtenerFormaOrden(), modoDeOrden());
+        }
     } else{
         alert('Valor no valido, intente de nuevo');
-        ordenarArray(obtenerFormaOrden());
+        ordenarArray(obtenerFormaOrden(), modoDeOrden());
     }
 }
 
-agregarProducto();
+function printObject() {
+    
+  }
+
+agregarProducto();  
